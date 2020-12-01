71c3371a11f2606b00033bf0ba9fee2f1368e7cb
Chris Mears <chris.mears@redbubble.com>
2020-12-01 21:33:14 +1100

    Add array version to day 1.

--- advent-of-code/2020/day-1/Expense.hs
+++ advent-of-code/2020/day-1/Expense.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE MultiWayIf #-}
 
+import Data.Array.Unboxed
+import Data.Array.IArray
 import Data.List
 import Data.Maybe
 import Data.Time
@@ -10,21 +12,31 @@ main = do
   xs <- (map read . lines <$> readFile "input.txt") :: IO [Integer]
   measure "input" $ print (sum xs)
 
-  -- Find 2 that sum to 2020, slow way.
-  measure "2 slow" $ print [ (x,y, x*y) | x <- xs, y <- xs, x+y == 2020 ]
+  let target :: Integral a => a
+      target = 2020
 
-  -- Find 3 that sum to 2020, slow way.
-  measure "3 slow" $ print [ (x,y,z, x*y*z) | x <- xs, y <- xs, z <- xs, x+y+z == 2020 ]
+  -- Find 2 that sum to target, slow way.
+  measure "2 slow" $ print $ listToMaybe [ (x,y, x*y) | x <- xs, y <- xs, x+y == target ]
 
-  -- Find 2 that sum to 2020, fast way.
-  measure "2 fast" $ case fast2 2020 xs of
+  -- Find 3 that sum to target, slow way.
+  measure "3 slow" $ print $ listToMaybe [ (x,y,z, x*y*z) | x <- xs, y <- xs, z <- xs, x+y+z == target ]
+
+  -- Find 2 that sum to target, fast way.
+  measure "2 fast" $ case fast2 target xs of
                        Nothing -> putStrLn "no solution"
                        Just (x,y) -> print (x,y,x*y)
 
-  -- Find 3 that sum to 2020, fast way.
-  measure "3 fast" $ case fast3 2020 xs of
+  -- Find 3 that sum to target, fast way.
+  measure "3 fast" $ case fast3 target xs of
                        Nothing -> putStrLn "no solution"
                        Just (x,y,z) -> print (x,y,z,x*y*z)
+
+  -- Find 3 that sum to target, faster (?) way.
+  measure "3 faster" $
+          let a = listArray (1, length xs) (map fromInteger (sort xs))
+          in case faster3 target a of
+               Nothing -> putStrLn "no solution"
+               Just (x,y,z) -> print (x,y,z,x*y*z)
          
 
 -- Assumes there's no duplicate numbers (e.g. 1010 and 1010).
@@ -53,6 +65,24 @@ loop t (x:xs) (y:ys) =
           | s <  t -> loop t xs (y:ys)
           | s >  t -> loop t (x:xs) ys
 loop _ _ _ = Nothing
+
+faster3 :: Int -> UArray Int Int -> Maybe (Int, Int, Int)
+faster3 t a = loop m
+  where
+    (m,n) = bounds a
+    loop i | i > n = Nothing
+    loop i =
+      let x1 = a!i
+          t' = t - x1
+          loop2 j k | j >= k = Nothing
+                    | otherwise =
+            let s = a!j + a!k
+            in if | s == t' -> Just (a!i,a!j,a!k)
+                  | s < t' -> loop2 (j+1) k
+                  | otherwise -> loop2 j (k-1)
+      in case loop2 (i+1) n of
+           Just x -> Just x
+           Nothing -> loop (i+1)
 
 measure s a = do
   t1 <- getCurrentTime
