ac8731066ee998ee26e9f0727be02ab4827dd75e
Chris Mears <chris@cmears.id.au>
2020-12-22 18:16:59 +1100

    Further simplify day 22 part 2.

--- 2020/day-22/Combat.hs
+++ 2020/day-22/Combat.hs
@@ -1,8 +1,5 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE ViewPatterns #-}
 
-import Control.Monad.State
-import qualified Data.Map as M
 import qualified Data.Set as S
 
 p1 = [23,32,46,47,27,35,1,16,37,50,15,11,14,31,4,38,21,39,26,22,3,2,8,45,19]
@@ -23,49 +20,24 @@ go (x:xs) (y:ys) | x > y = go (xs ++ [x,y]) ys
 
 type Config = ([Int],[Int])
 type GameMemory = S.Set Config
-type St = StateT GameMemory IO
-
-logEnabled = False
 
 -- Evaluate a game/round of recursive combat.
 -- Positive score if player 1 won, negative score if player 2 won.
--- "depth" is the depth of recursion for nicer output.
 -- The game memory records which configurations we have seen in *this* game (for the infinite-avoidance rule).
-go2 :: Int -> Config -> St Int
-go2 depth (xs,ys) = do
-  game <- get
+go2 :: GameMemory -> Config -> Int
+go2 game (xs,ys) = do
   case (xs,ys) of
-    -- Player 1 wins.
-    (_,[]) -> pure $ score xs
-    -- Player 1 wins by the infinite-avoidance rule.
-    _ | S.member (xs,ys) game -> pure $ score xs
-    -- Player 2 wins.
-    ([],_) -> pure $ -score ys
-    -- No one has won yet.
-    (x:xs',y:ys') -> do
-      -- Figure out who won this round.  (True for player 1, False for player 2)
-      p1wins <- do
-        -- If both players have enough cards to recurse...
-        if length xs' >= x && length ys' >= y
-        then do let subconfig = (take x xs', take y ys')
-                log $ "recursive: " ++ show subconfig
-                -- Play the subconfig a "fresh" game, with an empty "game" memory.
-                s <- liftIO $ evalStateT (go2 (depth+2) subconfig) S.empty
-                -- Return the score.
-                pure (s > 0)
-        -- If not enough cards to recurse, just compare the first two cards.
-        else pure (x > y)
-      -- Collect the cards and move to the next round.
-      modify $ S.insert (xs,ys)
-      if p1wins
-        then go2 depth ((xs'++[x,y]), ys')
-        else go2 depth (xs', (ys'++[y,x]))
-      
-  where
-    align = putStr (replicate depth ' ')
-    log s | logEnabled = liftIO $ align >> putStrLn s
-          | otherwise = pure ()
+    (_,[]) -> score xs
+    ([],_) -> -score ys
+    _ | S.member (xs,ys) game -> score xs
+    (x:xs',y:ys') ->
+      let game' = S.insert (xs,ys) game
+          p1wins = if length xs' >= x && length ys' >= y
+                   then go2 S.empty (take x xs', take y ys') > 0
+                   else x > y
+          config = if p1wins then ((xs'++[x,y]), ys') else (xs', (ys'++[y,x]))
+      in go2 game' config
 
 main = do
   print $ go p1 p2
-  print =<< evalStateT (go2 0 (p1,p2)) S.empty
+  print $ go2 S.empty (p1,p2)
