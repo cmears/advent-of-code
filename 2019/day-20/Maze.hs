69e58f0b0b9cc7ae5f37693952ee34d5ccfcc8f6
Chris Mears <chris@cmears.id.au>
2020-12-26 16:51:16 +1100

    Add day 20, part 2.

--- 2019/day-20/Maze.hs
+++ 2019/day-20/Maze.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ViewPatterns #-}
+
+-- The solution for part 2 imposes an arbitrary limit on the recursion
+-- depth, which is incorrect in general (but gives the right answer
+-- for my input).  A better may might be to compute the distances
+-- between portals on the same layer, and then search over that graph.
 import Control.Monad
 import Data.Char
 import Data.List
@@ -7,6 +12,7 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 import qualified Data.Sequence as Seq
 import Data.Sequence (ViewL(..), viewl)
+import Debug.Trace
 
 toMap :: String -> M.Map (Int,Int) Char
 toMap s = M.fromList $ do
@@ -29,30 +35,42 @@ main = do
            Nothing -> []
       coordToPortal = M.fromList portals
       portalToCoords = M.fromListWith (++) [ (p,[c]) | (c,p) <- portals ]
-      startCoord = head (portalToCoords M.! "AA")
-      endCoord = head (portalToCoords M.! "ZZ")
-      neighbourMap = M.fromList $ do
-                       ((r,c),x) <- M.toList maze
-                       guard (x == '.')
-                       let adjacents = do
-                                    coord <- [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
-                                    guard (f coord == '.')
-                                    pure coord
-                       let viaPortal = do
-                                    Just p <- pure (M.lookup (r,c) coordToPortal)
-                                    coord <- portalToCoords M.! p
-                                    guard (coord /= (r,c))
-                                    pure coord
-                       pure ((r,c), viaPortal ++ adjacents)
+      dotCoords = [ (r,c) | ((r,c),x) <- M.toList maze, x == '.' ]
+      (minR, maxR) = (minimum (map fst dotCoords), maximum (map fst dotCoords))
+      (minC, maxC) = (minimum (map snd dotCoords), maximum (map snd dotCoords))
+      startCoord = (\(r,c) -> (r,c,0)) $ head (portalToCoords M.! "AA")
+      endCoord = (\(r,c) -> (r,c,0)) $ head (portalToCoords M.! "ZZ")
+      neighbourFunc part (r,c,l) =
+          let adjacents = do
+                coord <- [(r+1,c,l),(r-1,c,l),(r,c+1,l),(r,c-1,l)]
+                guard (f (r,c) == '.')
+                pure coord
+              viaPortal = do
+                Just p <- pure (M.lookup (r,c) coordToPortal)
+                (pr,pc) <- portalToCoords M.! p
+                guard ((pr,pc) /= (r,c))
+                let pl = if part == 1
+                         then l
+                         else if r == minR || c == minC || r == maxR || c == maxC
+                              then l-1
+                              else l+1
+                guard (pl >= 0)
+                guard (pl <= 40)
+                pure (pr,pc,pl)
+          in viaPortal ++ adjacents
 --  mapM_ print (M.toList neighbourMap)
-  let depthMap = M.fromList $ bfs neighbourMap startCoord
-  print $ depthMap M.! endCoord
-  print endCoord
+  let depthMap = M.fromList $ bfs (neighbourFunc 1) startCoord
+  print $ snd $ depthMap M.! endCoord
 
-  let path c | c == startCoord = [c]
-             | otherwise = c : path (fst (depthMap M.! c))
-  let p = S.fromList (path endCoord)
-  printMaze maze p
+  let depthMap2 = M.fromList $ bfs (neighbourFunc 2) startCoord
+  print $ snd $ depthMap2 M.! endCoord
+--  print endCoord
+
+  -- let path c | c == startCoord = [c]
+  --            | otherwise = c : path (fst (depthMap2 M.! c))
+  -- let p = path endCoord
+  -- mapM_ print p
+  -- printMaze maze p
 
 
 printMaze maze path = do
@@ -72,12 +90,11 @@ pretty c = c
                        
 --  print portals
 
-bfs :: (Eq a, Ord a) => M.Map a [a] -> a -> [(a,(a,Int))]
-bfs neighbourMap start = loop (Seq.singleton (start,(start,0))) S.empty
+bfs :: (Eq a, Ord a) => (a -> [a]) -> a -> [(a,(a,Int))]
+bfs neighbourFunc start = loop (Seq.singleton (start,(start,0))) S.empty
   where
     loop (viewl -> EmptyL) visited = []
     loop (viewl -> (coord,(prev,depth)) :< q) visited =
         if S.member coord visited
         then loop q visited
-        else (coord,(prev,depth)) : loop (q <> Seq.fromList (map (,(coord,depth+1)) (neighbourMap M.! coord))) (S.insert coord visited)
-        
+        else (coord,(prev,depth)) : loop (q <> Seq.fromList (map (,(coord,depth+1)) (neighbourFunc coord))) (S.insert coord visited)
