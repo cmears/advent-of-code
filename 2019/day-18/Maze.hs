cd591dcea104454a8379d8c88570f90d4022a481
Chris Mears <chris@cmears.id.au>
2020-12-26 13:45:51 +1100

    Add day 18, part 2.

--- 2019/day-18/Maze.hs
+++ 2019/day-18/Maze.hs
@@ -1,3 +1,5 @@
+-- Inefficient: takes about 70 seconds to run when compiled.
+
 import Control.Monad.State
 import Data.Char
 import Data.List
@@ -5,23 +7,26 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 import System.IO.Unsafe
 
-import Debug.Trace
-
 type C = (Int,Int)
 
 type Maze = M.Map C Char
 
--- Where am I?
+-- Where are we?
 -- What keys have I collected?
-type S = (C, [Char])
+type S = ([C], [Char])
 
--- From this state, what keys can I reach (and how far away are they)?
+updateI :: Int -> a -> [a] -> [a]
+updateI i x xs = take i xs ++ [x] ++ drop (i+1) xs
+
+-- From this state, what keys can we reach (and how far away are they)?
 -- Won't return keys that are only reachable through other keys.
-flood :: Maze -> S -> [(Char, C, Int)]
-flood maze (startC, keys) = loop [(startC,0)] S.empty
+flood :: Maze -> S -> [(Char, [C], Int)]
+flood maze (startCs, keys) = do
+    (i,startC) <- zip [0..] startCs
+    (k,finalC,d) <- loop [(startC,0)] S.empty
+    pure (k,updateI i finalC startCs,d)
   where
     loop [] visited = []
---    loop ((c,d):q) visited | trace (show c) False = undefined
     loop ((c,d):q) visited | c `S.member` visited = loop q visited
                            | otherwise =
       let visited' = S.insert c visited
@@ -48,21 +53,18 @@ path maze s = evalState (loop s) M.empty
     nkeys = length $ filter isLower (M.elems maze)
     loop :: S -> State (M.Map S Int) Int
     loop (_,keys) | length keys == nkeys = pure 0
-    loop s | trace (show (s, length (snd s), nkeys)) False = undefined
     loop s = do
       cache <- get
       case M.lookup (sortS s) cache of
-        Just d -> --trace ("cached: " ++ show (d,nkeys)) $
-                    pure d
+        Just d -> pure d
         Nothing -> do
             let moves = flood maze s
                 -- For each move, calculate the best completion.
-            augmentedMoves <- mapM (\(k,c,d) -> do
-                                     d' <- loop (c, k:snd s)
-                                     pure (k,c,d,d')) moves
+            augmentedMoves <- mapM (\(k,cs,d) -> do
+                                     d' <- loop (cs, k:snd s)
+                                     pure (k,cs,d,d')) moves
                 -- Take the score of the best move + completion.
-            let d = minimum [ d+d' | (k,c,d,d') <- augmentedMoves ]
---            trace ("RECORDING " ++ show (s,d)) $
+            let d = minimum [ d+d' | (k,cs,d,d') <- augmentedMoves ]
             modify (M.insert (sortS s) d)
             pure d
 
@@ -75,14 +77,12 @@ readInput s = M.fromList $ do
                 (x,a) <- zip [1..] l
                 pure ((x,y),a)
 
-ex = unsafePerformIO (readFile "input.txt")
-maze = readInput ex
-
-go = do
-  let m = readInput ex
-      startC = head [ c | (c,a) <- M.toList m, a == '@' ]
-      s = (startC, "")
-  print $ flood m s
+go input = do
+  let m = readInput input
+      startCs = [ c | (c,a) <- M.toList m, a == '@' ]
+      s = (startCs, "")
   print $ path m s
 
-main = go
+main = do
+  go =<< readFile "input.txt"
+  go =<< readFile "input2.txt"
