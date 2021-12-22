c5ccb35f08f09c280078fb3361bf313cf6d3acc3
Chris Mears <chris@cmears.id.au>
2021-12-23 00:57:02 +1100

    Day 22, improved

--- haskell/2021/Day22.hs
+++ haskell/2021/Day22.hs
@@ -1,6 +1,8 @@
 import Data.List.Split
 
-parse :: String -> (Bool, ((Integer,Integer),(Integer,Integer),(Integer,Integer)))
+type Cube = ((Integer,Integer),(Integer,Integer),(Integer,Integer))
+
+parse :: String -> (Bool, Cube)
 parse line =
     let [onoff, range] = words line
         [x,y,z] = map (\s -> let [a,b] = splitOn ".." (drop 2 s) in (read a,read b)) (splitOn "," range)
@@ -9,35 +11,18 @@ parse line =
 main = do
   cubes <- map parse . lines <$> readFile "input22"
   let p1 = filter (\(_, (x,y,z)) -> all (\(a,b) -> -50 <= a && b <= 50) [x,y,z])
-  mapM_ (\f -> print . sum . map volume . processAll [] $ f cubes) [p1, id]
+  mapM_ (\f -> print . sum . map volume . foldl process [] $ f cubes) [p1, id]
 
 volume (x,y,z) = f x * f y * f z where f (l,u) = u-l+1
 
-processAll cubes [] = cubes
-processAll cubes (bcube:bcubes) =
-    case process cubes bcube of
-      Updated cubes' -> processAll cubes' bcubes
-      Split cs bcs -> processAll cs (bcs ++ bcubes)
+process existing (b,new) = (if b then [new] else []) ++ concatMap f existing
+  where f e = case intersection3 e new of
+                Just i -> filter (\sc -> not (sc `within` new)) (cleave i e)
+                _ -> [e]
 
-type Cube = ((Integer,Integer),(Integer,Integer),(Integer,Integer))
-type BCube = (Bool, Cube)
-data Result = Updated [Cube] | Split [Cube] [BCube]
-  deriving (Show)
+a `within` b = intersection3 a b == Just a
 
-process :: [Cube] -> BCube -> Result
-process existing (b,(x2,y2,z2)) = loop existing []
-  where
-    loop [] acc = if b then Updated ((x2,y2,z2):acc) else Updated acc
-    loop ((x1,y1,z1):es) acc =
-        case map intersection [(x1,x2),(y1,y2),(z1,z2)] of
-          [Just xi, Just yi, Just zi] ->
-              if b && (xi,yi,zi) == (x2,y2,z2) then Updated existing
-              else if not b && (xi,yi,zi) == (x1,y1,z1) then loop es acc
-                   else let ones = cleave (xi,yi,zi) (x1,y1,z1)
-                            twos = cleave (xi,yi,zi) (x2,y2,z2)
-                        in Split (ones++acc++es) (map (b,) twos)
-          _ -> loop es ((x1,y1,z1):acc)
-          
+intersection3 (x1,y1,z1) (x2,y2,z2) = (,,) <$> intersection (x1,x2) <*> intersection (y1,y2) <*> intersection (z1,z2)
 intersection ((l1,u1),(l2,u2)) | u1 < l2 = Nothing
                                | u2 < l1 = Nothing
                                | otherwise = Just (max l1 l2, min u1 u2)
