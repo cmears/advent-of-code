fbcd8bc5dafb09f5b1aa06f340f839a35056146c
Chris Mears <chris@cmears.id.au>
2022-12-01 22:04:43 +1100

    Golf some more

--- haskell/2022/1.hs
+++ haskell/2022/1.hs
@@ -1,7 +1,5 @@
+import Control.Arrow
 import Data.List
 import Data.List.Split
 
-main = do 
-  elves <- map (sum . map read) . splitOn [""] . lines <$> readFile "1.txt"
-  print . maximum $ elves
-  print . sum . take 3 . sortOn negate $ elves
+main = print =<< (head &&& sum) . take 3 . sortOn negate . map (sum . map read) . splitOn [""] . lines <$> readFile "1.txt"
