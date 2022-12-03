4f4c52d7997f83a4cdb6a40eb41b6e9ffe5379c5
Chris Mears <chris@cmears.id.au>
2022-12-03 16:53:46 +1100

    More golf

--- haskell/2022/3.hs
+++ haskell/2022/3.hs
@@ -5,7 +5,6 @@ import Data.List.Split
 main = do
   sacks <- lines <$> readFile "3.txt"
   let p cs = fromJust (head cs `elemIndex` "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") + 1
-      f1 sack = p . uncurry intersect . splitAt (length sack `div` 2) $ sack
-      f2 = p . foldl1 intersect
-  print . sum . map f1 $ sacks
-  print . sum . map f2 . chunksOf 3 $ sacks
+  print . sum . map (p . uncurry intersect . (splitAt . (`div` 2) =<< length)) $ sacks
+  print . sum . map (p . foldl1 intersect) . chunksOf 3 $ sacks
+
