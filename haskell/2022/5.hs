1fabeaf190200b8da51c7234f27195f9f73694df
Chris Mears <chris@cmears.id.au>
2022-12-05 18:40:11 +1100

    Slightly simplify

--- haskell/2022/5.hs
+++ haskell/2022/5.hs
@@ -1,14 +1,12 @@
-import Control.Monad.State
 import qualified Data.Map as M
 
 initS = M.fromList [ (1, "JFCNDBW"), (2, "TSLQVZP"), (3, "TJGBZP"), (4, "CHBZJLTD"), (5, "SJBVG"), (6, "QSP"), (7, "NPMLFDVB"), (8, "RLDBFMSP"), (9, "RTDV") ]
 parseLine l = let [_,n,_,f,_,t] = words l in (read n, read f, read t)
 
-execute :: (Int,Int,Int) -> State (M.Map Int String) ()
-execute (n,f,t) = modify (\m -> let (xs,ys) = splitAt n (m M.! f) in M.insert f ys . M.update (Just . (xs++)) t $ m)
+execute :: M.Map Int String -> (Int,Int,Int) -> M.Map Int String
+execute m (n,f,t) = let (xs,ys) = splitAt n (m M.! f) in M.insert f ys . M.update (Just . (xs++)) t $ m
 
 main = do
   commands <- map parseLine . lines <$> readFile "5.txt"
-  let go = print . map head . M.elems . flip execState initS . mapM execute
+  let go = print . map head . M.elems . foldl execute initS
   go (concatMap (\(n,f,t) -> replicate n (1,f,t)) commands) >> go commands
-
