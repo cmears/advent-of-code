b58f0045d782f72f5db0a332970f40c160e24d3f
Chris Mears <chris@cmears.id.au>
2022-12-07 17:50:12 +1100

    Simplify

--- haskell/2022/7.hs
+++ haskell/2022/7.hs
@@ -1,17 +1,16 @@
-{-# LANGUAGE ViewPatterns #-}
 import qualified Data.Map as M
 import Data.List
 
-execute (cwd,fs) (words -> ["$", "cd", ".."]) = (tail cwd,fs)
-execute (cwd,fs) (words -> ["$", "cd", "/"]) = ([],fs)
-execute (cwd,fs) (words -> ["$", "cd", d]) = (d:cwd,fs)
-execute (cwd,fs) (words -> ["$", "ls"]) = (cwd,fs)
-execute (cwd,fs) (words -> ["dir", _]) = (cwd,fs)
-execute (cwd,fs) (words -> [sz, f]) = (cwd,M.insert (f:cwd) (read sz) fs)
+execute (cwd,fs) ["$", "cd", ".."] = (tail cwd,fs)
+execute (cwd,fs) ["$", "cd", "/"] = ([],fs)
+execute (cwd,fs) ["$", "cd", d] = (d:cwd,fs)
+execute (cwd,fs) ["$", "ls"] = (cwd,fs)
+execute (cwd,fs) ["dir", _] = (cwd,fs)
+execute (cwd,fs) [sz, f] = (cwd,M.insert (f:cwd) (read sz) fs)
 
-dirSize d = sum . map snd . filter ((d `isSuffixOf`) . fst) . M.toList
 main = do
-  fs <- snd . foldl execute ([],M.empty) . lines <$> readFile "7.txt"
-  let sizes = map (flip dirSize fs) . nub . concatMap (tails . tail) $ M.keys fs
+  fs <- snd . foldl execute ([],M.empty) . map words . lines <$> readFile "7.txt"
+  let dirSize d = sum . map snd . filter ((d `isSuffixOf`) . fst) $ M.toList fs
+  let sizes = map dirSize . nub . concatMap (tails . tail) $ M.keys fs
   print . sum . filter (<=10^5) $ sizes
-  print . minimum . filter (>=(dirSize [] (fs) - 40*10^6)) $ sizes
+  print . minimum . filter (>=(dirSize [] - 40*10^6)) $ sizes
