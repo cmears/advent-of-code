758a89a1c12acd468cf68c782a19ae8ae10d6ef7
Chris Mears <chris@cmears.id.au>
2022-12-07 18:00:11 +1100

    Simplify

--- haskell/2022/7.hs
+++ haskell/2022/7.hs
@@ -6,11 +6,11 @@ execute (cwd,fs) ["$", "cd", "/"] = ([],fs)
 execute (cwd,fs) ["$", "cd", d] = (d:cwd,fs)
 execute (cwd,fs) ["$", "ls"] = (cwd,fs)
 execute (cwd,fs) ["dir", _] = (cwd,fs)
-execute (cwd,fs) [sz, f] = (cwd,M.insert (f:cwd) (read sz) fs)
+execute (cwd,fs) [sz, _] = (cwd,M.insertWith (+) cwd (read sz) fs)
 
 main = do
   fs <- snd . foldl execute ([],M.empty) . map words . lines <$> readFile "7.txt"
   let dirSize d = sum . map snd . filter ((d `isSuffixOf`) . fst) $ M.toList fs
-  let sizes = map dirSize . nub . concatMap (tails . tail) $ M.keys fs
+  let sizes = map dirSize . nub . concatMap tails $ M.keys fs
   print . sum . filter (<=10^5) $ sizes
   print . minimum . filter (>=(dirSize [] - 40*10^6)) $ sizes
