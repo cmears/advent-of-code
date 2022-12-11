534c8f1ced197b4f3aec0501497d24998378eca8
Chris Mears <chris@cmears.id.au>
2022-12-11 19:25:14 +1100

    Simplify

--- haskell/2022/11.hs
+++ haskell/2022/11.hs
@@ -13,17 +13,15 @@ step relief (monkeys,counts) idx =
   let m = monkeys `S.index` idx
   in case S.viewl (i m) of
        S.EmptyL -> (monkeys, counts)
-       item S.:< rest ->
-           let item' = relief (f m item)
-               recipient = ([n,y] !! (fromEnum (item' `mod` d m == 0))) m
-               m' = S.adjust' (\m -> m { i = i m S.|> item' }) recipient .
-                    S.update idx (m { i = rest}) $ monkeys
-           in step relief (m',S.adjust' (+1) idx counts) idx
+       item S.:< rest -> let item' = relief (f m item)
+                             recipient = ([n,y] !! (fromEnum (item' `mod` d m == 0))) m
+                             m' = S.adjust' (\m -> m { i = i m S.|> item' }) recipient .
+                                  S.update idx (m { i = rest }) $ monkeys
+                         in step relief (m',S.adjust' (+1) idx counts) idx
 
 main = do 
   let { monkeys = S.fromList input ; nm = S.length monkeys }
   let go (relief, n) =
         let states = iterate (\mc -> foldl (step relief) mc [0..nm-1]) (monkeys, S.replicate nm 0)
-        in product . take 2 . reverse . toList . S.sort . snd $ states !! n
-  print (go ((`div` 3), 20), go ((`mod` (product (map d (toList monkeys)))), 10000))
-
+        in product . S.take 2 . S.reverse . S.sort . snd $ states !! n
+  print (go ((`div` 3), 20), go ((`mod` product (d <$> monkeys)), 10000))
