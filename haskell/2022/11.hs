42ad386fc21d342b5c2a1cc35bb8f434d05fc8ec
Chris Mears <chris@cmears.id.au>
2022-12-11 19:16:41 +1100

    2022 day 11

--- haskell/2022/11.hs
+++ haskell/2022/11.hs
@@ -1,60 +1,29 @@
-import qualified Data.Map as M
-import Data.List
+import Data.Foldable
+import qualified Data.Sequence as S
 
-type Monkey = ([Integer], Integer->Integer, Integer, Int, Int)
-type M = M.Map Int Monkey
-type S = (M, M.Map Int Integer)
+data M = M { i :: S.Seq Int, f :: Int -> Int, d :: Int, y :: Int, n :: Int }
 
-real :: M
-real    = M.fromList $ zip [0..]
-            [ ([74,64,74,63,53], (*7), 5, 1, 6)
-            , ([69,99,95,62], (^2), 17, 2, 5)
-            , ([59,81], (+8), 7, 4, 3)
-            , ([50, 67, 63, 57, 63, 83, 97], (+4), 13, 0, 7)
-            , ([61, 94, 85, 52, 81, 90, 94, 70], (+3), 19, 7, 3)
-            , ([69], (+5), 3, 4, 2)
-            , ([54, 55, 58], (+7), 11, 1, 5)
-            , ([79, 51, 83, 88, 93, 76], (*3), 2, 0, 6)
-            ]
+input = [ M (s[74,64,74,63,53]) (*7) 5 1 6,M (s[69,99,95,62]) (^2) 17 2 5
+        , M (s[59,81]) (+8) 7 4 3,M (s[50,67,63,57,63,83,97]) (+4) 13 0 7
+        , M (s[61,94,85,52,81,90,94,70]) (+3) 19 7 3,M (s[69]) (+5) 3 4 2
+        , M (s[54,55,58]) (+7) 11 1 5,M (s[79,51,83,88,93,76]) (*3) 2 0 6
+        ] where s = S.fromList
 
-ex :: M
-ex = M.fromList $ zip [0..]
-     [ ([79,98], (*19), 23, 2, 3)
-     , ([54,65,75,74], (+6), 19, 2, 0)
-     , ([79,60,97], (^2), 13, 1, 3)
-     , ([74], (+3), 17, 0, 1)
-     ]
+step relief (monkeys,counts) idx =
+  let m = monkeys `S.index` idx
+  in case S.viewl (i m) of
+       S.EmptyL -> (monkeys, counts)
+       item S.:< rest ->
+           let item' = relief (f m item)
+               recipient = ([n,y] !! (fromEnum (item' `mod` d m == 0))) m
+               m' = S.adjust' (\m -> m { i = i m S.|> item' }) recipient .
+                    S.update idx (m { i = rest}) $ monkeys
+           in step relief (m',S.adjust' (+1) idx counts) idx
 
+main = do 
+  let { monkeys = S.fromList input ; nm = S.length monkeys }
+  let go (relief, n) =
+        let states = iterate (\mc -> foldl (step relief) mc [0..nm-1]) (monkeys, S.replicate nm 0)
+        in product . take 2 . reverse . toList . S.sort . snd $ states !! n
+  print (go ((`div` 3), 20), go ((`mod` (product (map d (toList monkeys)))), 10000))
 
-step :: S -> Int -> S
-step (monkeys,counts) i =
-  let (items, f, d, yes, no) = monkeys M.! i
-  in case items of
-       [] -> (monkeys, counts)
-       (item:rest) ->
-           let item' = f item `mod` (5*17*7*13*19*3*11*2)
---           let item' = f item `mod` (23*19*13*17)
-               recip = if item' `mod` d == 0
-                       then yes
-                       else no
-               m' = M.update (\(ri,rf,rd,ry,rn) -> Just (ri++[item'],rf,rd,ry,rn)) recip .
-                    M.insert i (rest,f,d,yes,no) $
-                    monkeys
-               c' = M.update (Just . (+1)) i counts
-           in step (m',c') i
-
-play (monkeys, counts) = foldl step (monkeys, counts) [0..((M.size monkeys) - 1)]
-
-showItems monkeys = do
-  mapM_ (\(j,(i,f,d,y,n)) -> putStrLn $ show j ++ ": " ++ show i) (M.toList monkeys)
-
-main = do
-  let monkeys = real
-      states = iterate play $ (monkeys, M.fromList (zip [0..(M.size monkeys)-1] (repeat 0)))
-      items = map (\(m,c) -> map (\(i,_,_,_,_) -> i) (M.elems m)) states
---  showItems (fst (states !! 15))
---  print (snd (states !! 20))
-  let counts = M.elems $ snd (states !! 10000)
-  let (a:b:_) = reverse $ sort $ counts
-  print counts
-  print $ a*b
