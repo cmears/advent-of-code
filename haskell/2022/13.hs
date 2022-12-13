842879bc2a5496526b3c962562d16879a9f06951
Chris Mears <chris@cmears.id.au>
2022-12-13 18:14:32 +1100

    Simplify more

--- haskell/2022/13.hs
+++ haskell/2022/13.hs
@@ -7,11 +7,11 @@ data P = L [P] | I Int deriving (Show, Eq)
 instance Ord P where compare = compareP
 
 parse s = let (p, "") = parseP s in p
-parseP ('[':rest) = (let (ys,r) = parseTail rest in (L ys,r))
-parseP xs = first I (head (reads xs))
-parseTail (',':xs) = parseTail xs
-parseTail (']':rest) = ([], rest)
-parseTail xs = let { (p,rest) = parseP xs ; (ys, r) = parseTail rest } in (p:ys, r)
+parseP ('[':rest) = first L $ parseList rest
+parseP xs = first I . head $ reads xs
+parseList (',':xs) = parseList xs
+parseList (']':rest) = ([], rest)
+parseList xs = uncurry first . bimap (:) parseList $ parseP xs
 
 compareP (I x) (I y) = compare x y
 compareP (L xs) (L ys) = compare xs ys
