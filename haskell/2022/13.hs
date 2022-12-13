06f4a09381442e600f68c214b8f71b60016a8895
Chris Mears <chris@cmears.id.au>
2022-12-13 18:08:17 +1100

    Some simplification

--- haskell/2022/13.hs
+++ haskell/2022/13.hs
@@ -1,66 +1,25 @@
-import Data.Char
+import Data.Bifunctor
 import Data.List
 import Data.List.Split
+import Data.Maybe
 
-data P = L [P] | I Int
-  deriving (Show, Eq)
+data P = L [P] | I Int deriving (Show, Eq)
+instance Ord P where compare = compareP
 
-parse s = let (p, "") = parseP s
-          in p
-
-parseP :: String -> (P, String)
-parseP ('[':rest) = parseHead rest
-parseP xs = let (ds,rest) = span isDigit xs
-            in (I (read ds), rest)
-
-parseHead (']':rest) = (L [], rest)
-parseHead xs =
-    let (p,rest) = parseP xs
-        (ys, r) = parseTail rest
-    in (L (p:ys), r)
-
--- parseTail (',':xs) =
---     let (ds,rest) = span isDigit xs
---         h = I (read ds)
---         (ys, r) = parseTail rest
---     in ((h:ys), r)
--- parseTail (']':rest) = ([], rest)
-
-parseTail (',':xs) =
-    let (p,rest) = parseP xs
-        (ys,r) = parseTail rest
-    in (p:ys, r)
+parse s = let (p, "") = parseP s in p
+parseP ('[':rest) = (let (ys,r) = parseTail rest in (L ys,r))
+parseP xs = first I (head (reads xs))
+parseTail (',':xs) = parseTail xs
 parseTail (']':rest) = ([], rest)
+parseTail xs = let { (p,rest) = parseP xs ; (ys, r) = parseTail rest } in (p:ys, r)
 
-inorder :: P -> P -> Ordering
-inorder (I x) (I y) = compare x y
-inorder (L xs) (L ys) = compareLists xs ys
-inorder (I x) (L ys) = inorder (L [I x]) (L ys)
-inorder (L xs) (I y) = inorder (L xs) (L [I y])
-
-compareLists :: [P] -> [P] -> Ordering
-compareLists (x:xs) (y:ys) =
-  case inorder x y of
-    LT -> LT
-    GT -> GT
-    EQ -> compareLists xs ys
-compareLists [] (y:ys) = LT
-compareLists (x:xs) [] = GT
-compareLists [] [] = EQ
-
-part1 = do
-  xs <- map (map parse) . splitOn [""] . lines <$> readFile "13otavio.txt"
-  let indexes = map fst $ filter (\(i,[x,y]) -> inorder x y == LT) $ zip [1..] xs
-  print $ xs !! 38
-  print indexes
-  print $ sum indexes
+compareP (I x) (I y) = compare x y
+compareP (L xs) (L ys) = compare xs ys
+compareP (I x) (L ys) = compare (L [I x]) (L ys)
+compareP (L xs) (I y) = compare (L xs) (L [I y])
 
 main = do
-  xs0 <- map parse . filter (not . null) . lines <$> readFile "13.txt"
-  let xs = L [L [I 2]] : L [L [I 6]] : xs0
---  print xs
-  let sorted = sortBy inorder xs
-  let Just a = findIndex (\p -> p == L [L [I 2]]) sorted
-  let Just b = findIndex (\p -> p == L [L [I 6]]) sorted
-  print $ (a+1) * (b+1)
---  mapM_ print sorted
+  ps <- map parse . filter (not . null) . lines <$> readFile "13.txt"
+  print . sum . map fst . filter (\(_,[x,y]) -> x < y) . zip [1..] $ chunksOf 2 ps
+  let dividers = parse <$> ["[[2]]","[[6]]"]
+  print . product $ (\d -> succ . fromJust . findIndex (==d) $ sort (dividers++ps)) <$> dividers
