1aadfacfb24839f4ff113061d1e53dc5a45f63b0
Chris Mears <chris@cmears.id.au>
2022-12-15 21:19:52 +1100

    Tidy

--- haskell/2022/15.hs
+++ haskell/2022/15.hs
@@ -6,8 +6,6 @@ import Util
 parseLine :: String -> ((Int, Int), (Int, Int))
 parseLine (submatches "Sensor at x=(.*), y=(.*): closest beacon is at x=(.*), y=(.*)" -> Just [a,b,c,d]) = ((read a,read b),(read c, read d))
 
-manhattan (a,b) (x,y) = abs (a-x) + abs (b-y)
-
 coveredRange (sx,sy) sr targety | radius <= 0 = Nothing
                                 | otherwise = Just (sx-radius,sx+radius)
   where radius = sr - abs (targety - sy)
@@ -19,10 +17,10 @@ ins ((a,b):rest) (x,y) | y < a-1 = (x,y) : (a,b) : rest
 
 main = do
   pairs <- map parseLine . lines <$> readFile "15.txt"
-  let triples = [(s,b,d) | (s,b) <- pairs, let d = manhattan s b]
-  let ranges row = foldl ins [] $ mapMaybe (\(s,b,d) -> coveredRange s d row) triples
-  let beacons = nub . filter ((==2*10^6).snd) $ (\(s,b,d) -> b) <$> triples
-  print (sum (map (\(x,y) -> y-x+1) (ranges 2000000)) - length beacons)
+  let triples = [(s,b,abs (sx-bx) + abs (sy-by)) | (s@(sx,sy),b@(bx,by)) <- pairs ]
+      ranges row = foldl ins [] $ mapMaybe (\(s,b,d) -> coveredRange s d row) triples
+      beacons = length . nub . filter ((==2*10^6).snd) $ (\(s,b,d) -> b) <$> triples
+  print . subtract beacons . sum $ (\(x,y) -> y-x+1) <$> ranges (2*10^6)
   print . head $ mapMaybe (\r -> case ranges r of
                                    [_] -> Nothing
                                    [(_,a),(b,_)] -> Just (r+4000000*(a+1))) [1..4*10^6]
