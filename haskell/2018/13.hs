d19774f6b92e9f8448d49c6d96fd962bf1d376bb
Chris Mears <chris@cmears.id.au>
2023-07-30 20:29:15 +1000

    2018 day 13 part 2

--- haskell/2018/13.hs
+++ haskell/2018/13.hs
@@ -3,6 +3,7 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.List
 import Debug.Trace
+import Data.Maybe
 
 type Coord = (Int, Int)
 
@@ -13,12 +14,11 @@ main = do
     linePairs <- zip [0..] . lines <$> readFile "13.txt"
     let pairs :: [(Coord, Char)]
         pairs = [((x,y),c) | (y,l) <- linePairs, (x,c) <- zip [0..] l]
-    let (m,l) = parsePairs pairs
-    -- mapM_ print $ M.toList m
-    -- print l
-    print $ simulate (m,l)
-    pure ()
+    let s = parsePairs pairs
+    print $ simulate1 s
+    print $ simulate2 s
 
+parsePairs :: [(Coord, Char)] -> S
 parsePairs pairs = loop pairs (M.empty, [])
   where
     loop [] s = s
@@ -36,15 +36,15 @@ parsePairs pairs = loop pairs (M.empty, [])
                     _ -> (m,l)
         in loop ps s'
 
-simulate :: S -> Coord
-simulate (m,l) = --trace (showS (m,l)) $
+simulate1 :: S -> Coord
+simulate1 (m,l) = --trace (showS (m,l)) $
   --trace (show l) $
     let occupied = S.fromList (map (\(coord,_,_) -> coord) l)
         toProcess = sortOn (\((x,y),c,t) -> (y,x)) l
 
     in case loop toProcess occupied [] of
          Left coord -> coord
-         Right xs -> simulate (m,xs)
+         Right xs -> simulate1 (m,xs)
     where
         loop [] _ acc = Right acc
         loop ((coord,c,t):xs) occupied acc =
@@ -52,6 +52,25 @@ simulate (m,l) = --trace (showS (m,l)) $
             in if S.member coord' occupied
                 then Left coord'
                 else loop xs (S.insert coord' (S.delete coord occupied)) ((coord',c',t'):acc)
+
+simulate2 :: S -> Coord
+simulate2 (m,[(coord,_,_)]) = coord
+simulate2 (m,l) = -- trace (showS (m,l)) $
+  --trace (show l) $
+    let occupied = S.fromList (map (\(coord,_,_) -> coord) l)
+        toProcess = sortOn (\((x,y),c,t) -> (y,x)) l
+        indexed = M.fromList $ zip [0..] toProcess
+
+    in simulate2 (m, M.elems (loop (M.keys indexed) indexed))
+    where
+        loop [] idx = idx
+        loop (i:is) idx | isNothing (M.lookup i idx) = loop is idx
+        loop (i:is) idx =
+            let (coord,c,t) = idx M.! i
+                (coord',c',t') = move m (coord,c,t)
+            in case find (\j -> let (cc,_,_) = idx M.! j in cc == coord') (M.keys idx) of
+                Just j -> loop is (M.delete i . M.delete j $ idx)
+                Nothing -> loop is (M.insert i (coord',c',t') idx)
 
 move :: M.Map Coord Char -> (Coord, Char, Int) -> (Coord, Char, Int)
 move m ((x,y),c,t) =
