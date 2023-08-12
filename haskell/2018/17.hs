027b540d39b75b4c584306664504f7bab9fb994a
Chris Mears <chris@cmears.id.au>
2023-08-12 21:50:00 +1000

    2018 day 17 array version

--- haskell/2018/17.hs
+++ haskell/2018/17.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE RankNTypes #-}
 import Control.Monad.State
 import qualified Data.Set as S
 import Data.Set (Set)
 import Data.List.Split
+
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.MArray
+import Data.Array.Unboxed
 
 import Debug.Trace
 
@@ -26,60 +32,81 @@ parsePart s = case splitOn ".." (drop 2 s) of
 main = do
   coords <- concatMap parseLine . lines <$> readFile "17.txt"
   let clay = S.fromList coords
-  let s = fill clay
-  let (S w r) = s
-  print $ S.size w
-  print $ S.size r
-
-data S = S { wet :: Set Coord, rest :: Set Coord }
-  deriving (Show)
+  let (w,r) = fill clay
+  let nwet = length $ filter id $ elems w
+      nrest = length $ filter id $ elems r
+  print nwet
+  print nrest
+  print $ S.size clay
+  putStrLn ""
+  print $ nwet - S.size clay
+  print $ nrest - S.size clay
+  pure ()
 
 data Result = IntoTheVoid | Pooled
 data Probe = Overflow | Wall Int
 
-fill :: Set Coord -> S
-fill clay = execState (loop spring) (S S.empty S.empty)
+type A s = STUArray s Coord Bool
+
+fill :: Set Coord -> (UArray Coord Bool, UArray Coord Bool)
+fill clay = runST $ do
+              let minCoord = (minX-1, minY)
+                  maxCoord = (maxX+1, maxY)
+              wa <- trace (show (minCoord, maxCoord)) $
+                  newArray (minCoord, maxCoord) False
+              ra <- newArray (minCoord, maxCoord) False
+              forM_ (S.toList clay) $ \c -> do
+                writeArray wa c True
+                writeArray ra c True
+              loop spring wa ra
+              wa' <- freeze wa
+              ra' <- freeze ra
+              pure (wa', ra')
   where
     minY = minimum (snd <$> S.toList clay)
     maxY = maximum (snd <$> S.toList clay)
-    loop coord = do
-      (S w r) <- get
-      if coord `S.member` clay || coord `S.member` r then pure Pooled
-      else if snd coord > maxY then pure IntoTheVoid
+    minX = minimum (fst <$> S.toList clay)
+    maxX = maximum (fst <$> S.toList clay)
+    loop :: forall s. Coord -> A s -> A s -> ST s Result
+    loop coord wa ra | snd coord > maxY = pure IntoTheVoid
+    loop coord wa ra | snd coord < minY = probeDown coord wa ra
+    loop coord wa ra = do
+      isRest <- readArray ra coord
+      if isRest then pure Pooled
       else do
-         markWet coord
-         dres <- probeDown coord
+         markWet wa coord
+         dres <- probeDown coord wa ra
          case dres of
            IntoTheVoid -> pure IntoTheVoid
            Pooled -> do
-                     lres <- probeLeft coord
-                     rres <- probeRight coord
+                     lres <- probeLeft coord wa ra
+                     rres <- probeRight coord wa ra
                      case (lres, rres) of
                        (Wall a, Wall b) -> do
                                          let coords = [ (x,snd coord) | x <- [a+1..b-1] ]
-                                         mapM_ markRest coords
+                                         mapM_ (markRest ra) coords
                                          pure Pooled
                        _ -> pure IntoTheVoid
-    probeLR :: Int -> Coord -> State S Probe
-    probeLR offset (x,y) = do
+    probeLR :: Int -> Coord -> A s -> A s -> ST s Probe
+    probeLR offset (x,y) wa ra = do
         let next = (x+offset, y)
-        (S w r) <- get
-        if next `S.member` clay then pure (Wall (x+offset))
-        else if next `S.member` r then error "?!?!?!?!?"
-             else do
-               markWet next
-               dres <- probeDown next
-               case dres of
-                 IntoTheVoid -> pure Overflow
-                 Pooled -> probeLR offset next
+        isRest <- readArray ra next
+        if isRest then pure (Wall (x+offset))
+        else do
+          markWet wa next
+          dres <- probeDown next wa ra
+          case dres of
+            IntoTheVoid -> pure Overflow
+            Pooled -> probeLR offset next wa ra
     probeLeft = probeLR (-1)
     probeRight = probeLR 1
 
+    probeDown :: Coord -> A s -> A s -> ST s Result
     probeDown (x,y) = loop (x,y+1)
 
-    markWet :: Coord -> State S ()
-    markWet coord | snd coord < minY = pure ()
-                  | otherwise = modify (\(S w r) -> (S (S.insert coord w) r))
+    markWet :: A s -> Coord -> ST s ()
+    markWet wa coord | snd coord < minY = pure ()
+                     | otherwise = writeArray wa coord True
 
-    markRest :: Coord -> State S ()
-    markRest coord = modify (\(S w r) -> (S w (S.insert coord r)))
+    markRest :: A s -> Coord -> ST s ()
+    markRest ra coord = writeArray ra coord True
