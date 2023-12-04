db963292e3dbbcb4729f0d62fe463c1df1d155f5
Chris Mears <chris@cmears.id.au>
2023-12-04 17:25:47 +1100

    2023 day 4 improved more

--- haskell/2023/4.hs
+++ haskell/2023/4.hs
@@ -2,6 +2,6 @@ import Data.List
 import Data.List.Split
 
 main = do
-  xs <- map (\[a,b] -> length (intersect a (b :: [Int]))) . map (map (map read . words) . splitOn "|" . (!!1) . splitOn ":") . lines <$> readFile "4.txt"
+  xs <- map (\[a,b] -> length (intersect a b)) . map (map words . splitOn "|" . (!!1) . splitOn ":") . lines <$> readFile "4.txt"
   print . sum $ ((`div`2) . (2^)) <$> xs
   print . sum . map head . init $ scanl (\(m:mults) c -> zipWith (+) (replicate c m ++ repeat 0) mults) (repeat 1) xs
