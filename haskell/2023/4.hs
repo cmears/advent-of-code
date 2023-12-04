ff4685e38195a27f4d2c9d10b5a2ecc436d4a00d
Chris Mears <chris@cmears.id.au>
2023-12-04 16:54:27 +1100

    2023 day 4 shortened

--- haskell/2023/4.hs
+++ haskell/2023/4.hs
@@ -1,32 +1,8 @@
-import Control.Monad.State
 import Data.List
 import Data.List.Split
-import qualified Data.Map as M
 
 main = do
-  ls <- lines <$> readFile "4.txt"
-  let cards = M.fromList $ zip [1..] (map readCard ls)
-  print cards
-  let n = length ls
-
-  let b = flip execState (M.fromList [(i,1)|i<-[1..n]]) $ do
-                forM_ [1..n] $ \i -> do
-                  m <- get
-                  let mult = m M.! i
-                  let c = cards M.! i
-                  let x = scoreCard c
-                  forM_ [i+1..i+x] $ \j -> do
-                    modify (M.adjust (+mult) j)
-  print $ sum $ M.elems b
-
-
-readCard :: String -> ([Int],[Int])
-readCard l =
-  let [_,r] = splitOn ":" l
-      [a,b] = splitOn "|" r
-  in (map read (words a), map read (words b))
-
-scoreCard (winning, numbers) =
-  let i = intersect winning numbers
-      n = length i
-  in n
+  cards <- (((((read <$>) . words) <$>) . splitOn "|" . (!!1) . splitOn ":") <$>) . lines <$> readFile "4.txt" :: IO [[[Int]]]
+  let f [a,b] = length (intersect a b)
+  print . sum $ ((`div`2) . (2^) . f) <$> cards
+  print . sum . map head . init $ scanl (\(m:mults) c -> zipWith (+) (replicate (f c) m ++ repeat 0) mults) (repeat 1) cards
