46fa220ba325add858b8f6338445bd4c81b74457
Chris Mears <chris@cmears.id.au>
2023-12-06 22:15:59 +1100

    2023 day 6 correction

--- haskell/2023/6.hs
+++ haskell/2023/6.hs
@@ -1,5 +1,5 @@
 input = [("53","333"),("83","1635"),("72","1289"),("88","1532")]
 main = print (f input) >> print (f [((\(a,b) -> (concat a, concat b)) (unzip input))])
   where f input = product (g <$> input)
-        g (t,r) = let [x,y] = q (read t,read r) in floor x - ceiling y + 1
+        g (t,r) = let [x,y] = q (read t,read r) in ceiling x - floor y - 1
         q (t,r) = [ (t `op` sqrt (t^2 - 4*r)) / 2 | op <- [(+),(-)] ]
