7b891badf0fdab104a963130350f3760b6458acf
Chris Mears <chris@cmears.id.au>
2023-12-07 19:29:54 +1100

    2023 day 7 v2

--- haskell/2023/7.hs
+++ haskell/2023/7.hs
@@ -2,10 +2,8 @@ import Data.List
 import Data.Ord
 
 main = do
-  pairs <- map (\l -> let [a,b] = words l in (map charToCard a, read b)) . lines <$> readFile "7.txt"
+  pairs <- map (\l -> let [a,b] = words l in (map (head . (`elemIndices` "0123456789TJQKA")) a, read b)) . lines <$> readFile "7.txt"
   let pairs2 = map (\(h,b) -> (map (\c -> if c == 11 then 1 else c) h, b)) pairs
   mapM_ (print . sum . zipWith (*) [1..] . map snd . sortBy (comparing (judgeHand . fst) <> compare)) [pairs, pairs2]
-charToCard = head . (`elemIndices` "0123456789TJQKA")
-judgeHand = f . reverse . sort . map snd . count . filter (/= 1)
+judgeHand = f . reverse . sort . map snd . map (\g -> (head g, length g)) . group . sort . filter (/= 1)
   where { f [] = [5] ; f (c:cs) = (c+(5-sum (c:cs)):cs) }
-count = map (\g -> (head g, length g)) . group . sort
