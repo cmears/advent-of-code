b5c653a2d078b8ff953dc65f58ccfb00b356d653
Chris Mears <chris@cmears.id.au>
2023-12-19 22:44:38 +1100

    2023 day 19 part 2

--- haskell/2023/19.hs
+++ haskell/2023/19.hs
@@ -1,27 +1,25 @@
-import Data.List
 import Data.List.Split (splitOn)
 import Data.Maybe
 import Text.Trifecta
 import qualified Data.Map as M
+import qualified Data.Set as S
 
-data Workflow = Workflow [Rule] deriving Show
+type Workflow = [Rule]
 data Rule = Rule Condition Consequence deriving Show
-data Condition = Always | Comparison Op String Integer deriving Show
+data Condition = Always | Comparison Op Char Integer deriving Show
 data Op = Less | Greater deriving Show
 data Consequence = Accept | Reject | Redirect String deriving Show
 
-data Part = Part (M.Map String Integer) deriving Show
+type Part = M.Map Char Integer
 
 parseWorkflow :: Parser Workflow
-parseWorkflow = Workflow <$> braces (sepBy1 parseRule comma)
+parseWorkflow = braces (sepBy1 parseRule comma)
 
 parseRule :: Parser Rule
 parseRule = Rule <$> option Always (try (parseCondition <* char ':')) <*> parseConsequence
 
-parseCategory :: Parser String
-parseCategory = do
-  c <- satisfy (`elem` "xmas")
-  pure [c]
+parseCategory :: Parser Char
+parseCategory = satisfy (`elem` "xmas")
 
 parseCondition :: Parser Condition
 parseCondition = do
@@ -42,16 +40,11 @@ parseNamedWorkflow :: Parser (String, Workflow)
 parseNamedWorkflow = (,) <$> many letter <*> parseWorkflow
 
 parsePart :: Parser Part
-parsePart = do
-  pairs <- braces (sepBy parseProperty comma)
-  pure $ Part $ foldl f M.empty pairs
+parsePart = foldl f M.empty <$> braces (sepBy parseProperty comma)
   where f m (c,n) = M.insert c n m
 
-parseProperty :: Parser (String, Integer)
+parseProperty :: Parser (Char, Integer)
 parseProperty = (,) <$> parseCategory <*> (char '=' *> decimal)
-
-ex = "xy{a<2006:qkq,m>2090:A,rfg}"
-ex2 = "{x=2036,m=264,a=79,s=2244}"
 
 parse p s = case parseString p mempty s of
               Success a -> a
@@ -59,31 +52,52 @@ parse p s = case parseString p mempty s of
 
 parseInput :: String -> (M.Map String Workflow, [Part])
 parseInput s = 
-    let ls = lines s
-        [workflows, parts] = splitOn [""] ls
-    in (M.fromList (map (fromSuccess . parseString parseNamedWorkflow mempty) workflows),
-        map (fromSuccess . parseString parsePart mempty) parts)
-
-fromSuccess (Success a) = a
+    let [workflows, parts] = splitOn [""] (lines s)
+    in (M.fromList (map (parse parseNamedWorkflow) workflows), map (parse parsePart) parts)
 
 data Evaluation = Accepted | Rejected deriving (Show, Eq)
 
 evaluate :: M.Map String Workflow -> Part -> Evaluation
 evaluate workflows part = evaluateWorkflow (workflows M.! "in") part
-  where evaluateWorkflow (Workflow rules) part =
+  where evaluateWorkflow rules part =
             case head $ mapMaybe (\r -> evaluateRule r part) rules of
               Accept -> Accepted
               Reject -> Rejected
               Redirect s -> evaluateWorkflow (workflows M.! s) part
         evaluateRule (Rule Always consequence) part = Just consequence
-        evaluateRule (Rule (Comparison op cat n) cons) (Part ratings) | (f op) (ratings M.! cat) n = Just cons
+        evaluateRule (Rule (Comparison op cat n) cons) part | (f op) (part M.! cat) n = Just cons
         evaluateRule _ _ = Nothing
         f Less = (<)
         f Greater = (>)
 
---simulate :: Map.Map String Workflow -> 
+simulate :: M.Map String Workflow -> Integer
+simulate workflows =
+  let rules = (workflows M.! "in")
+  in simulate' (M.fromList [(c,S.fromList [1..4000]) | c <- "xmas"]) rules
+  where
+    simulate' :: M.Map Char (S.Set Integer) -> [Rule] -> Integer
+    simulate' v _ | any S.null (M.elems v) = 0
+    simulate' v ((Rule condition consequence):rs) =
+        let (v1,v2) = applyCondition condition v
+        in case consequence of
+             Accept -> sz v1 + simulate' v2 rs
+             Reject -> simulate' v2 rs
+             Redirect s -> simulate' v1 (workflows M.! s) + simulate' v2 rs
+
+applyCondition :: Condition -> M.Map Char (S.Set Integer) -> (M.Map Char (S.Set Integer), M.Map Char (S.Set Integer))
+applyCondition Always v = (v, M.fromList [(c,S.empty) | c <- "xmas"])
+applyCondition (Comparison op c n) v =
+    let (smaller, equal, bigger) = S.splitMember n (v M.! c)
+        (s1,s2') = case op of
+                     Less -> (smaller, bigger)
+                     Greater -> (bigger, smaller)
+        s2 = if equal then S.insert n s2' else s2'
+    in (M.insert c s1 v, M.insert c s2 v)
+
+sz = product . map (fromIntegral . S.size) . M.elems
 
 main = do
   (workflows, parts) <- parseInput <$> readFile "19.txt"
   let accepted = filter ((==Accepted) . evaluate workflows) parts
-  print $ sum $ map (\(Part ratings) -> sum (M.elems ratings)) accepted
+  print $ sum $ map (sum . M.elems) accepted
+  print $ simulate workflows
